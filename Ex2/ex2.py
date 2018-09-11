5b149097ea59a4f1db0b725b91a6e07826cbfb6e
felipeard <felipe_ard96@hotmail.com>
2018-09-11 18:02:06 -0300

    Ex2 iniciado

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -2,3 +2,17 @@
 #	Felipe Alegria Rollo Dias	nUSP 9293501
 #	Leonardo Piccaro Rezende	nUSP 9364611
 #
+
+class MLP(object):
+
+	"""docstring for MLP"""
+	def __init__(self):
+		# Iniatialize MLP class
+
+	def architecture(self):
+
+	def foward(self):
+
+	def backpropagation(self):
+
+	
