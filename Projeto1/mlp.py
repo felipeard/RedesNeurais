4ce3036a01a59301af691fadd09c9581ea2eeb11
felipeard <felipe_ard96@hotmail.com>
2018-09-20 17:41:14 -0300

    Projeto finalizado

--- Projeto1/mlp.py
+++ Projeto1/mlp.py
@@ -14,7 +14,9 @@ class MLP(object):
 
 	# Dictionary do python para armazenar o modelo da MLP
 	model = {}
-	
+	dE2_dw_h_b = 0
+	dE2_dw_o_b = 0
+
 	# Construtor da classe
 	def __init__(self):
 		# Iniatialize MLP class
@@ -76,7 +78,7 @@ class MLP(object):
 		}
 
 	# Realiza o treinamento da rede utilizando backpropagation com regra delta
-	def backpropagation(self,X,Y,eta=0.5,max_error=0.000001,max_iter=500):
+	def backpropagation(self,X,Y,eta=0.5,alpha=0.5,max_error=0.000001,max_iter=500):
 		counter = 0
 		total_error = 2*max_error
 
@@ -108,12 +110,18 @@ class MLP(object):
 				delta_h = np.matmul(np.array([delta_out]),self.model['output_layer'][:,0:self.model['hidden_lenght']])
 				dE2_dw_h = delta_h * (np.multiply(-2*fw['df_net_h'],np.array([np.concatenate((x_i,np.array([1])))]).T))
 
+
 				# Atualização dos pesos
-				#print('x_i.size=',x_i.size,'\n')
-				#print('bag loko=',np.reshape(np.array([eta*dE2_dw_h]).T,(self.model['hidden_lenght'],x_i.size+1)),'\n')
-				self.model['output_layer'] = self.model['output_layer'] - eta*dE2_dw_o
-				self.model['hidden_layer'] = self.model['hidden_layer'] - np.reshape(np.array([eta*dE2_dw_h]).T,(self.model['hidden_lenght'],x_i.size+1))
+				self.model['output_layer'] = self.model['output_layer'] - eta*dE2_dw_o - alpha*self.dE2_dw_o_b
+				if counter == 0:
+					self.model['hidden_layer'] = self.model['hidden_layer'] - np.reshape(np.array([eta*dE2_dw_h]).T,(self.model['hidden_lenght'],x_i.size+1))
+				else:
+					self.model['hidden_layer'] = self.model['hidden_layer'] - np.reshape(np.array([eta*dE2_dw_h]).T,(self.model['hidden_lenght'],x_i.size+1)) - np.reshape(np.array([alpha*self.dE2_dw_h_b]).T,(self.model['hidden_lenght'],x_i.size+1))
 				
+
+				self.dE2_dw_o_b = dE2_dw_o
+				self.dE2_dw_h_b = dE2_dw_h
+
 			# Término da iteração do treinamento
 			total_error = total_error/X.shape[0]
 			counter = counter+1
@@ -122,7 +130,7 @@ class MLP(object):
 
 		return
 
-	def run(self,X,Y,size=2,eta=0.1,max_iter=500,train_size=0.7,threshold=0.000001):
+	def run(self,X,Y,task,size=8,eta=0.1,alpha=0.5,max_iter=500,train_size=0.7,threshold=0.000001):
 		ids = random.sample(range(0,X.shape[0]),np.floor(train_size*X.shape[0]).astype(np.int))
 		ids_left = diff(range(0,X.shape[0]),ids)
 		#print('ids',ids,'\n')
@@ -142,31 +150,35 @@ class MLP(object):
 
 		self.architecture(input_lenght=X.shape[1],hidden_lenght=size,output_lenght=Y.shape[1])
 		print('MLP architecture created\nStarting Training')
-		self.backpropagation(train_set,train_classes,eta=eta,max_error=threshold,max_iter=max_iter)
+		self.backpropagation(train_set,train_classes,eta=eta,alpha=alpha,max_error=threshold,max_iter=max_iter)
 		print('Neural Network Trained\nStarting Testing')
 		#print(mlp.forward(X)['f_net_o'])
 
 		correct = 0
+		sqerror = 0
 		for i in range(0,test_set.shape[0]):
 			x_i = test_set[i]
 			y_i = test_classes[i]
 
-
-			y_hat_i = np.round(self.forward(x_i)['f_net_o'])
-			#print('y_hat_i',y_hat_i,'\n')
-			if (np.sum((y_i - y_hat_i)**2) == 0):
-				correct = correct + 1
+			if task == 'C':
+				y_hat_i = np.round(self.forward(x_i)['f_net_o'])
+			if task == 'R':
+				y_hat_i = self.forward(x_i)['f_net_o']
 			
+			error = y_i - y_hat_i
+			if (np.sum((error)**2) == 0):
+				correct = correct + 1
+			sqerror = sqerror + np.sum(error*error)
 
 			pass
 
 		print('Neural Network Tested')
 		accuracy = correct/test_set.shape[0]
-		error = np.sum((y_i - y_hat_i)**2)/test_set.shape[0]
+		sqerror = sqerror/test_set.shape[0]
 		
 		return {
 			"accuracy": accuracy,
-			"error": error
+			"error": sqerror
 		}
 	#END OF MLP CLASS
 
@@ -189,52 +201,59 @@ def diff(first, second):
         second = set(second)
         return [item for item in first if item not in second]
 
-def wine_test(eta=0.1,max_iter=500,train_size=0.7):
+def wine_test(eta=0.1,alpha=0,max_iter=500,train_size=0.7):
 	for file in os.listdir():
 		if(file.endswith('.data')):
+			# Preprocessing wine data set
 			data = open(file).read()
 			X = matrix(data)
 			X = np.array(X)
 			X = X.astype(np.float)
 			Y = X[:,0]
 			X = X[:,1:X.shape[1]]
-			#print('X=',X)
+			# Normalizing X
 			for i in range(X.shape[1]):
 				X[:,i] = (X[:,i] - np.amin(X[:,i])) / (np.amax(X[:,i]) - np.amin(X[:,i]))
-			#X = scale(X)
+			
+			# Binarizing the classes output
 			Y = class_ind(Y)
-			#print('X=',X)
-			#print('Y=',Y)
+			
 
 	print('\nPreprocessing Wine Done')
 	mlp = MLP()
-	return mlp.run(X,Y,eta=eta,max_iter=max_iter,train_size=train_size)
+	return mlp.run(X,Y,'C',eta=eta,alpha=alpha,max_iter=max_iter,train_size=train_size)
 
-def tracks_test(eta=0.1,max_iter=500,train_size=0.7):
+def tracks_test(eta=0.1,alpha=0.5,max_iter=500,train_size=0.7):
 	mlp = MLP()
 	n = 1
 	ret = {}
 	for file in os.listdir():
 		if(file.endswith('.txt')):
+			# Preprocessing the files for Origin of Music
 			data = open(file).read()
 			X = matrix(data)
 			X = np.array(X)
 			X = X.astype(np.float)
 			Y = X[:,X.shape[1]-2:X.shape[1]]
 			X = X[:,0:X.shape[1]-2]
+
+			# Normalizing X and Y
 			for i in range(X.shape[1]):
 				X[:,i] = (X[:,i] - np.amin(X[:,i])) / (np.amax(X[:,i]) - np.amin(X[:,i]))
 			for i in range(Y.shape[1]):
 				Y[:,i] = (Y[:,i] - np.amin(Y[:,i])) / (np.amax(Y[:,i]) - np.amin(Y[:,i]))
-			#print('X=',X.shape[1])
-			#print('Y=',Y.shape[1])
-			print('\nPreprocessing Origin of Music Done')
-			res = mlp.run(X,Y,eta=eta,max_iter=max_iter,train_size=train_size)
-			#print('Error=',res['error'])
+			
+			print('\nPreprocessing Origin of Music file ',n,' Done')
+			res = mlp.run(X,Y,'R',eta=eta,alpha=alpha,max_iter=max_iter,train_size=train_size)
+			
+
+			# Saving the error for each file in different positions
 			if n == 1:
 				ret['error_1'] = res['error']
 			if n == 2:
 				ret['error_2'] = res['error']
+
+			# Adding the counter of file
 			n = n+1
 
 	return ret
@@ -253,31 +272,39 @@ op = int(op)
 if op == 1: # If wine database is chosen
 	print('Wine Choosen')
 	table = PrettyTable()
-	table.field_names = ["Number of Cycles","Learning Speed","Training set size","Accuracy"]
+	table.field_names = ["Number of Cycles","Learning Speed","Momentum","Training set size","Accuracy"]
 
 	# Variation Learning Speed
 	ret = wine_test(eta=0.1)
-	table.add_row([500,0.1,0.7,ret['accuracy']])
+	table.add_row([500,0.1,0,0.7,ret['accuracy']])
 	ret = wine_test(eta=0.3)
-	table.add_row([500,0.3,0.7,ret['accuracy']])
+	table.add_row([500,0.3,0,0.7,ret['accuracy']])
 	ret = wine_test(eta=0.5)
-	table.add_row([500,0.5,0.7,ret['accuracy']])
+	table.add_row([500,0.5,0,0.7,ret['accuracy']])
 
 	# Variating Number of Cycles
 	ret = wine_test(max_iter=250)
-	table.add_row([250,0.1,0.7,ret['accuracy']])
+	table.add_row([250,0.1,0,0.7,ret['accuracy']])
 	ret = wine_test(max_iter=750)
-	table.add_row([750,0.1,0.7,ret['accuracy']])
+	table.add_row([750,0.1,0,0.7,ret['accuracy']])
 	ret = wine_test(max_iter=1000)
-	table.add_row([1000,0.1,0.7,ret['accuracy']])
+	table.add_row([1000,0.1,0,0.7,ret['accuracy']])
 
 	# Variating Training set size
 	ret = wine_test(train_size=0.5)
-	table.add_row([500,0.1,0.5,ret['accuracy']])
+	table.add_row([500,0.1,0,0.5,ret['accuracy']])
 	ret = wine_test(train_size=0.6)
-	table.add_row([500,0.1,0.6,ret['accuracy']])
+	table.add_row([500,0.1,0,0.6,ret['accuracy']])
 	ret = wine_test(train_size=0.9)
-	table.add_row([500,0.1,0.9,ret['accuracy']])
+	table.add_row([500,0.1,0,0.9,ret['accuracy']])
+
+	# Variation Momentum
+	ret = wine_test(alpha=0.1)
+	table.add_row([500,0.1,0.1,0.7,ret['accuracy']])
+	ret = wine_test(alpha=0.3)
+	table.add_row([500,0.1,0.3,0.7,ret['accuracy']])
+	ret = wine_test(alpha=0.7)
+	table.add_row([500,0.1,0.7,0.7,ret['accuracy']])
 
 	# Printing Results
 	print(table)
@@ -285,30 +312,39 @@ if op == 1: # If wine database is chosen
 elif op == 2:
 	print('Origin of Music Choosen')
 	table = PrettyTable()
-	table.field_names = ["Number of Cycles","Learning Speed","Training set size","First file Mean Square Error","Second file Mean Square Error"]
+	table.field_names = ["Number of Cycles","Learning Speed","Momentum","Training set size","First file Mean Square Error","Second file Mean Square Error"]
 
 	#Variating Learning Speed
 	err = tracks_test(eta=0.1)
-	table.add_row([500,0.1,0.7,err['error_1'],err['error_2']])
+	table.add_row([500,0.1,0.5,0.7,err['error_1'],err['error_2']])
 	err = tracks_test(eta=0.3)
-	table.add_row([500,0.3,0.7,err['error_1'],err['error_2']])
+	table.add_row([500,0.3,0.5,0.7,err['error_1'],err['error_2']])
 	err = tracks_test(eta=0.5)
-	table.add_row([500,0.5,0.7,err['error_1'],err['error_2']])
+	table.add_row([500,0.5,0.5,0.7,err['error_1'],err['error_2']])
 
 	#Variating Number of Cycles
-	err = tracks_test(max_iter=300)
-	table.add_row([300,0.1,0.7,err['error_1'],err['error_2']])
-	err = tracks_test(max_iter=500)
-	table.add_row([700,0.1,0.7,err['error_1'],err['error_2']])
-	err = tracks_test(max_iter=700)
-	table.add_row([1000,0.1,0.7,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,max_iter=300)
+	table.add_row([300,0.5,0.5,0.7,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,max_iter=500)
+	table.add_row([700,0.5,0.5,0.7,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,max_iter=700)
+	table.add_row([1000,0.5,0.5,0.7,err['error_1'],err['error_2']])
 
 	#Variating Training Set Size
-	err = tracks_test(train_size=0.5)
-	table.add_row([500,0.1,0.5,err['error_1'],err['error_2']])
-	err = tracks_test(train_size=0.75)
-	table.add_row([500,0.1,0.75,err['error_1'],err['error_2']])
-	err = tracks_test(train_size=0.9)
-	table.add_row([500,0.1,0.9,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,train_size=0.5)
+	table.add_row([500,0.5,0.5,0.5,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,train_size=0.75)
+	table.add_row([500,0.5,0.5,0.75,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,train_size=0.9)
+	table.add_row([500,0.5,0.5,0.9,err['error_1'],err['error_2']])
 
+	#Variating Momentum
+	err = tracks_test(eta=0.5,alpha=0.1)
+	table.add_row([500,0.5,0.1,0.7,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,alpha=0.3)
+	table.add_row([500,0.5,0.3,0.7,err['error_1'],err['error_2']])
+	err = tracks_test(eta=0.5,alpha=0.7)
+	table.add_row([500,0.5,0.7,0.7,err['error_1'],err['error_2']])
+
+	# Printing Results
 	print(table)
